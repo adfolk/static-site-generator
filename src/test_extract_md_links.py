98a6ad2065008c43da47f57aa77b22c6edf3f931
adfolk <adfolkestad@gmail.com>
2024-10-06 20:58:46 +0400

    implemented link extraction

---
+++ src/test_extract_md_links.py
@@ -0,0 +1,13 @@
+from extract_md_links import *
+import unittest
+
+class TestExtractLinks(unittest.TestCase):
+    def test_image_extraction(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        print(extract_markdown_images(text))
+# [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+
+    def test_link_extraction(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        print(extract_markdown_links(text))
+# [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
