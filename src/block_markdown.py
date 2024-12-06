72c931e66a96a7bf1e0a0cd9b2c9a3026c00dd4b
adfolk <adfolkestad@gmail.com>
2024-12-06 18:18:15 +0400

    Completed block type checking function and unit tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,10 +1,57 @@
+import re
+
 def markdown_to_blocks(markdown):
     raw_split = markdown.split('\n\n')
     clean_blocks = []
 
     for item in raw_split:
         if item != '':
-            clean_blocks.append(item)
+            clean_blocks.append(item.strip())
 
     return clean_blocks
+
+def block_to_block_type(md_block):
+    header_match = re.match(r'(#{1,6} )', md_block)
+    if header_match:
+        return 'heading'
+    
+    if md_block.startswith('```') and md_block.endswith('```\n'):
+        return 'code'
+
+    if md_block.startswith('>'):
+        lines = md_block.splitlines()
+        is_quote = True
+        for line in lines:
+            if line.startswith('>') == False:
+                is_quote = False
+                break
+        if is_quote:
+            return 'quote'
+
+    if md_block.startswith('* ') or md_block.startswith('- '):
+        lines = md_block.splitlines()
+        is_unord_list = True
+        for line in lines:
+            if line.startswith('* ') or line.startswith('- '):
+                continue
+            else:
+                is_unord_list = False
+                break
+        if is_unord_list:
+            return 'unordered_list'
+    
+    if md_block[0].isnumeric() and md_block.startswith('. ', 1):
+        lines = md_block.splitlines()
+        is_ord_list = True
+        num = 0
+        for line in lines:
+            if int(line[0]) == (num + 1) and line.startswith('. ', 1):
+                num += 1
+            else:
+                is_ord_list = False
+                break
+        if is_ord_list:
+            return 'ordered_list'
+    else:
+        return 'paragraph'
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -8,4 +8,28 @@ class TestBlockMarkdownFuncs(unittest.TestCase):
         print(markdown_to_blocks(input))
 
     #TODO: 1) multiple newline characters of even and odd numbers
+    def test_markdown_to_blocks_multiple_spaces(self):
+        input = "# This is a heading\n\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n"
+        print("#### test markdown blocks multi ####")
+        print(markdown_to_blocks(input))
+
+    def test_block_type_heading(self):
+        input = "### This is a heading"
+        self.assertEqual(block_to_block_type(input), 'heading')
+
+    def test_block_type_code(self):
+        input = "```\nThis is the first line of the code block\nThis is the second\nThis is the third\n```\n"
+        self.assertEqual(block_to_block_type(input), 'code')
+
+    def test_block_type_quote(self):
+        input = ">quote line 1\n>quote line 2\n>quote line 3"
+        self.assertEqual(block_to_block_type(input), 'quote')
+
+    def test_block_type_unordered_list(self):
+        input = "* list item 1\n* list item 2\n* list item 3"
+        self.assertEqual(block_to_block_type(input), 'unordered_list')
+
+    def test_block_type_ordered_list(self):
+        input = "1. list item 1\n2. list item 2\n3. list item 3"
+        self.assertEqual(block_to_block_type(input), 'ordered_list')
 
