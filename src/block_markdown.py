1fac3102cf6d2ab7728bb9f2ebb2c7176f2127e8
adfolk <adfolkestad@gmail.com>
2024-12-12 07:38:31 +0400

    wrote test for md to html function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,12 @@
 import re
 
+heading_type = 'heading'
+code_type = 'code'
+quote_type = 'quote'
+ul_type= 'unordered_list'
+ol_type = 'ordered_list'
+para_type = 'paragraph'
+
 def markdown_to_blocks(markdown):
     raw_split = markdown.split('\n\n')
     clean_blocks = []
@@ -13,23 +20,23 @@ def markdown_to_blocks(markdown):
 def block_to_block_type(md_block):
     header_match = re.match(r'(#{1,6} )', md_block)
     if header_match:
-        return 'heading'
-    
-    if md_block.startswith('```') and md_block.endswith('```\n'):
-        return 'code'
+        return heading_type
+
+    if md_block.startswith('```') and md_block.endswith('```'):
+        return code_type
+
+    lines = md_block.splitlines()
 
     if md_block.startswith('>'):
-        lines = md_block.splitlines()
         is_quote = True
         for line in lines:
             if line.startswith('>') == False:
                 is_quote = False
                 break
         if is_quote:
-            return 'quote'
+            return quote_type
 
     if md_block.startswith('* ') or md_block.startswith('- '):
-        lines = md_block.splitlines()
         is_unord_list = True
         for line in lines:
             if line.startswith('* ') or line.startswith('- '):
@@ -38,10 +45,9 @@ def block_to_block_type(md_block):
                 is_unord_list = False
                 break
         if is_unord_list:
-            return 'unordered_list'
-    
+            return ul_type
+
     if md_block[0].isnumeric() and md_block.startswith('. ', 1):
-        lines = md_block.splitlines()
         is_ord_list = True
         num = 0
         for line in lines:
@@ -51,7 +57,10 @@ def block_to_block_type(md_block):
                 is_ord_list = False
                 break
         if is_ord_list:
-            return 'ordered_list'
+            return ol_type
     else:
-        return 'paragraph'
+        return para_type
+
+def markdown_to_html_node(markdown):
+    pass
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,15 +1,17 @@
 import unittest
 from block_markdown import *
+from htmlnode import *
+from textnode import *
 
 class TestBlockMarkdownFuncs(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        input = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n"
+        input = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
         print("#### test markdown to blocks ####")
         print(markdown_to_blocks(input))
 
     #TODO: 1) multiple newline characters of even and odd numbers
     def test_markdown_to_blocks_multiple_spaces(self):
-        input = "# This is a heading\n\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n"
+        input = "# This is a heading\n\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
         print("#### test markdown blocks multi ####")
         print(markdown_to_blocks(input))
 
@@ -18,7 +20,7 @@ class TestBlockMarkdownFuncs(unittest.TestCase):
         self.assertEqual(block_to_block_type(input), 'heading')
 
     def test_block_type_code(self):
-        input = "```\nThis is the first line of the code block\nThis is the second\nThis is the third\n```\n"
+        input = "```\nThis is the first line of the code block\nThis is the second\nThis is the third\n```"
         self.assertEqual(block_to_block_type(input), 'code')
 
     def test_block_type_quote(self):
@@ -32,4 +34,39 @@ class TestBlockMarkdownFuncs(unittest.TestCase):
     def test_block_type_ordered_list(self):
         input = "1. list item 1\n2. list item 2\n3. list item 3"
         self.assertEqual(block_to_block_type(input), 'ordered_list')
+
+    def test_markdown_to_html_node(self):
+        input = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+
+        expected_output = HTMLNode(
+            tag='div',
+            children = [
+                HTMLNode(
+                    tag='h1',
+                    value='This is a heading',
+                    children = [
+                        HTMLNode(
+                            tag='p',
+                            children = [
+                                LeafNode('This is a paragraph of text. It has some '),
+                                LeafNode('bold', tag='b'),
+                                LeafNode(' and '),
+                                LeafNode('italic', tag='i'),
+                            ]
+                        ),
+                        HTMLNode(
+                            tag='ul',
+                            children = [
+                                LeafNode('This is the first list item in a list block'),
+                                LeafNode('This is a list item'),
+                                LeafNode('This is another list item'),
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
+        self.assertEqual(expected_output, markdown_to
+
+
 
