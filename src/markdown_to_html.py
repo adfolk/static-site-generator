cbc8276dd0f7f453fc656f09c46210d91cba95b3
adfolk <adfolkestad@gmail.com>
2025-01-01 20:45:23 +0400

    partial progress on markdown to html

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -4,7 +4,21 @@ from inline_markdown import *
 from block_markdown import *
 
 def get_header_lvl(md_line: str):
+# Returns the html tag name of a header level as a string, e.g., 'h1', 'h3', 'h6', etc.
+# Requires that you first use the block_to_block_type() from the block_markdown module to find out which blocks are headers
     space = md_line.find(' ')
     num_hashes = md_line[:space].count('#')
     return f'h{num_hashes}'
+
+def text_to_children(text, block_type):
+    if block_type == heading_type:
+
+
+
+
+def markdown_to_html(md_doc: str):
+    blocks = markdown_to_blocks(md_doc)
+    block_types = list(map(block_to_block_type, blocks))
+
+    for i in range(len(blocks)):
 
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -1,7 +1,22 @@
 import unittest
 from markdown_to_html import *
 
+simple_doc = '''
+### This is the header
+
+This is a paragraph of text. It has **bold** and *italic* text.
+
+```
+# This is a code block of python:
+print("Hello, world!")
+```
+
+'''
+
 class TestMarkdownToHTML(unittest.TestCase):
     def test_get_header_lvl(self):
         header = '#### This header has an "#" in it'
         self.assertEqual(get_header_lvl(header), 'h4')
+
+    def test_markdown_to_html(self):
+        print(markdown_to_html(simple_doc))
