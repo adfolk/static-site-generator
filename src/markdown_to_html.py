8d95004bff4b32437d6ef929a34bdb461812cd06
adfolk <adfolkestad@gmail.com>
2025-01-03 07:56:49 +0400

    First draft of text_to_children function and first test.

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -10,15 +10,16 @@ def get_header_lvl(md_line: str):
     num_hashes = md_line[:space].count('#')
     return f'h{num_hashes}'
 
-def text_to_children(text, block_type):
-    if block_type == heading_type:
+def text_to_children(text):
+# Takes in a string of markdown text and outputs a list of LeafNodes broken down by inline text type.
+# If a plain text paragraph block is encountered, this function should simply return None.
+    text_nodes = text_to_text_nodes(text)
+    if text_nodes != None:
+        for i in text_nodes:
+            i = text_node_to_html_node(i)
+    return text_nodes
 
-
-
-
-def markdown_to_html(md_doc: str):
-    blocks = markdown_to_blocks(md_doc)
-    block_types = list(map(block_to_block_type, blocks))
-
-    for i in range(len(blocks)):
+#def markdown_to_html(md_doc: str):
+#    blocks = markdown_to_blocks(md_doc)
+#    block_types = list(map(block_to_block_type, blocks))
 
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -1,7 +1,47 @@
 import unittest
 from markdown_to_html import *
 
-simple_doc = '''
+text_only_para_doc_md = '''
+This markdown doc is plain text.
+
+But it does have multiple paragraphs.
+
+Like this one.
+
+'''
+text_only_para_doc_html_nodes = [
+        LeafNode('This markdown doc is plain text.', tag='p'),
+        LeafNode('But it does have multiple paragraphs.', tag='p'),
+        LeafNode('Like this one.', tag='p')
+        ]
+
+mixed_para_doc_md = '''
+This markdown doc consists solely of paragraphs.
+
+The second paragraph has some **bold**, *italic*, and `inline code` text.
+
+The third paragraph is text again.
+
+'''
+
+mixed_para_doc_html_nodes = [
+        LeafNode('This markdown doc consists solely of paragraphs.'),
+        ParentNode(
+            'p',
+            [
+                LeafNode('The second paragraph has some'),
+                LeafNode('bold', tag='b'),
+                LeafNode(', '),
+                LeafNode('italic', tag='i'),
+                LeafNode(', and '),
+                LeafNode('inline code', tag='code'),
+                LeafNode('text.')
+            ]
+        ),
+        LeafNode('The third paragraph is text again.')
+    ]
+
+complex_doc_md = '''
 ### This is the header
 
 This is a paragraph of text. It has **bold** and *italic* text.
@@ -18,5 +58,7 @@ class TestMarkdownToHTML(unittest.TestCase):
         header = '#### This header has an "#" in it'
         self.assertEqual(get_header_lvl(header), 'h4')
 
-    def test_markdown_to_html(self):
-        print(markdown_to_html(simple_doc))
+    #def test_markdown_to_html(self):
+        #self.assertEqual(markdown_to_html(mixed_para_doc_md), )
+    def test_text_to_children(self):
+        self.assertEqual(text_to_children(text_only_para_doc_md), None)
